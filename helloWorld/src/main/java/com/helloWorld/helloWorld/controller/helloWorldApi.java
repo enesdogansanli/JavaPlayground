f1e643f086a7aa2d0934db1cf76f57f25ec8fe43
Enes Doğan ŞANLI <enesdogansanli@gmail.com>
2025-06-03 15:03:02 +0300

    Add hello world web app project

---
+++ helloWorld/src/main/java/com/helloWorld/helloWorld/controller/helloWorldApi.java
@@ -0,0 +1,16 @@
+package com.helloWorld.helloWorld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class helloWorldApi {
+	
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
