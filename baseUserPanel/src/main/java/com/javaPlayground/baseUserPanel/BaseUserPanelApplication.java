e8bc69734000f8d1317d4ba8dc737456ff4dc3ef
Enes Doğan ŞANLI <enesdogansanli@gmail.com>
2025-09-06 20:07:36 +0300

    Add base user panel project files from spring initializr

---
+++ baseUserPanel/src/main/java/com/javaPlayground/baseUserPanel/BaseUserPanelApplication.java
@@ -0,0 +1,13 @@
+package com.javaPlayground.baseUserPanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseUserPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseUserPanelApplication.class, args);
+	}
+
+}
